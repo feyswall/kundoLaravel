8bb9af80e436b736c39b38597ab7bcd78084fd6b
feyswall <vicentmkota91@gmail.com>
2023-01-22 12:04:11 +0300

    sms changes

--- app/Listeners/SendingSmsNotificationListener.php
+++ app/Listeners/SendingSmsNotificationListener.php
@@ -78,6 +78,46 @@ class SmsServicesControlller extends Controller
         }
         return ['status' => 'success', 'response' => $response];
     }
+
+
+    public function deriveryReport($dest_addr, $request_id){
+        $username = $this->api_key;
+        $password = $this->secret_key;
+
+        $URL = 'https://dlrapi.beem.africa/public/v1/delivery-reports';
+
+        $body = array('request_id' => $request_id, 'dest_addr' => $dest_addr);
+
+        // Setup cURL
+        $ch = curl_init();
+        error_reporting(E_ALL);
+        ini_set('display_errors', 1);
+
+        $URL = $URL . '?' . http_build_query($body);
+
+        curl_setopt($ch, CURLOPT_URL, $URL);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt_array($ch, array(
+            CURLOPT_HTTPGET => true,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HTTPHEADER => array(
+                'Authorization:Basic ' . base64_encode("$username:$password"),
+                'Content-Type: application/json',
+            ),
+        ));
+
+        // Send the request
+        $response = curl_exec($ch);
+
+        // Check for errors
+        if ($response === false) {
+            echo $response;
+
+            die(curl_error($ch));
+        }
+        var_dump($response);
+    }
 }
 
 ?>
