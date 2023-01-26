687c32946212df557a465b04a37b3d7e8c7770ed
shughurijr07 <abdumalikiandrew@gmail.com>
2023-01-26 17:39:59 +0300

    done:

--- resources/views/_partials/super/_super_header.blade.php
+++ resources/views/_partials/super/_super_header.blade.php
@@ -92,7 +92,7 @@
                 <button type="button" class="btn header-item waves-effect" id="page-header-user-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <img class="rounded-circle header-profile-user" src="assets/images/users/avatar-4e.png" alt="Header Avatar"> <span class="d-none d-xl-inline-block ms-1 fw-medium font-size-15">Super User</span> <i class="uil-angle-down d-none d-xl-inline-block font-size-15"></i> </button>
                 <div class="dropdown-menu dropdown-menu-end">
                     <!-- item-->
-                    <a class="dropdown-item" href="{{ route('profile.show', Auth::user()->id) }}"><i class="uil uil-user-circle font-size-18 align-middle text-muted me-1"></i> <span class="align-middle">View Profile</span></a> 
+                    <a class="dropdown-item" href="{{ route('profile.show') }}"><i class="uil uil-user-circle font-size-18 align-middle text-muted me-1"></i> <span class="align-middle">View Profile</span></a> 
                     <x-system.logout />
                 </div>
             </div>
--- resources/views/profile/user-profile.blade.php
+++ resources/views/profile/user-profile.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.super_system')
 
 @section('content')
-  <h1> user profile card should be here</h1>
+  <h1> user profile card should be here </h1>
 @endsection
