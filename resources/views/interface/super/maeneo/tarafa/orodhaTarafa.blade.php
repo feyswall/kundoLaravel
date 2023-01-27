31188fbcf9c334959f4b847c31f5696f897a396d
feyswall <vicentmkota91@gmail.com>
2023-01-27 14:58:47 +0300

    rekebisha form mbili

--- resources/views/interface/super/maeneo/tarafa/orodhaTarafa.blade.php
+++ resources/views/interface/super/maeneo/tarafa/orodhaTarafa.blade.php
@@ -112,48 +112,8 @@
                                                             </div>
                                                         </div>
                                                     </div>
-                                                    <div class="col-sm-12 col-md-4 col-lg-3">
-                                                        <div class="mb-3 mb-4">
-                                                            <label class="form-label" for="middleName">Jina La Kati</label>
-                                                            <input type="text" class="form-control" name="middleName" placeholder="eg: mosi">
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-sm-12 col-md-4 col-lg-3">
-                                                        <div class="mb-3 mb-4">
-                                                            <label class="form-label" for="lastName">Jila La Mwisho</label>
-                                                            <input type="text" class="form-control" name="lastName" placeholder="eg: mgalanga simo">
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-sm-12 col-md-4 col-lg-3">
-                                                        <div class="mb-3 mb-4">
-                                                            <label class="form-label" for="phone">Namba ya Simu</label>
-                                                            <input type="text" class="form-control" name="phone" placeholder="eg: 0678 987 897">
-
-                                                            <!-- data to simplify the validation process -->
-                                                            <input type="hidden" value="{{ $council->id }}" class="form-control" name="side_id">
-                                                            <input type="hidden" value="council_leader" class="form-control" name="table">
-                                                            <input type="hidden" value="council_id" class="form-control" name="side_column">
-
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-sm-12 col-md-4 col-lg-3">
-                                                        <div class="mb-3 mb-4">
-                                                            <label class="form-label" for="wadhifa">Chagua Wadhifa</label>
-                                                            <select class="form-control" name="post_id">
-                                                                @foreach( \App\Models\Post::where('area', 'halmashauri')->get() as $post )
-                                                                <option value="{{ $post->id }}">{{ $post->name }}</option>
-                                                                @endforeach
-                                                            </select>
-                                                        </div>
-                                                    </div>
-                                                    <div class="row">
-                                                        <div class="col-12">
-                                                            <button type="submit" name="submit" class="btn btn-primary btn-md">Ongeza</button>
-                                                        </div>
-                                                    </div>
-                                    </div>
-                                    </form>
-                                    </x-slot:content>
+                                                </form>
+                                            </x-slot:content>
                                     </x-system.modal>
                                 </div>
                             </div>
