7a672764bb05ce4e792ffb3a504236a9e54219cd
feyswall <vicentmkota91@gmail.com>
2023-06-05 07:10:52 -0700

    error solve

--- app/Http/Controllers/MotorOwner/MotorServicesController.php
+++ app/Http/Controllers/MotorOwner/MotorServicesController.php
@@ -76,7 +76,7 @@ class MotorServicesController extends Controller
             'owner_id' => $owner->id,
             'garage_id' => $request->garage,
         ]);
-        $message .= route('super.service.moja', $service->id );
+        $message .= route('super.services.moja', $service->id );
 
         foreach( $request->serviceType as $key => $type ){
             $typeObj = ServiceType::find($type);
