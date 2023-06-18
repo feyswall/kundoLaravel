ab0f8d2b669d421e1bf69a438d2ae5a0a0a5611f
feyswall <vicentmkota91@gmail.com>
2023-06-18 16:35:41 +0300

    area for all users

--- resources/views/interface/super/viongozi/singleLeader.blade.php
+++ resources/views/interface/super/viongozi/singleLeader.blade.php
@@ -38,8 +38,20 @@
                                         $leader_id,
                                         $area_type
                                     );
-
+                                    $branch = $area->branch;
+                                    $ward = $area->ward;
+                                    $division = $area->division;
+                                    $council = $area->council;
+                                    $district = $area->district;
+                                    $region = $area->region;
+                                    $area_stack = 'mkoa - '.$region->name.' | ';
+                                    $area_stack .= 'wilaya - '.$district->name.' | ';
+                                    $area_stack .= 'halmashauri - '.$council->name.' | ';
+                                    $area_stack .= 'tarafa - '.$division->name.' | ';
+                                    $area_stack .= 'kata - '.$ward->name.' | ';
+                                    $area_stack .= 'branch - '.$branch->name.' | ';
                                     if($area){
+                                        echo "<p>".$area_stack."</p>";
                                         echo $area_string.' <b>'.$area->name.'</b>';
                                     }
                                 @endphp
@@ -51,6 +63,28 @@
                                     $side_column = 'branch_id';
                                     $area_type = 'App\Models\Branch';
                                     $area_string = 'tawi la ';
+
+                                    $area =  App\Http\Controllers\Super\AreasController::search_for_area(
+                                        $relation_table,
+                                        $side_column,
+                                        $side_value,
+                                        $leader_id,
+                                        $area_type
+                                    );
+                                    $ward = $area->ward;
+                                    $division = $area->division;
+                                    $council = $area->council;
+                                    $district = $area->district;
+                                    $region = $area->region;
+                                    $area_stack = 'mkoa - '.$region->name.' | ';
+                                    $area_stack .= 'wilaya - '.$district->name.' | ';
+                                    $area_stack .= 'halmashauri - '.$council->name.' | ';
+                                    $area_stack .= 'tarafa - '.$division->name.' | ';
+                                    $area_stack .= 'kata - '.$ward->name.' | ';
+                                    if($area){
+                                        echo "<p>".$area_stack."</p>";
+                                        echo $area_string.' <b>'.$area->name.'</b>';
+                                    }
                                 @endphp
                             @endif
 
@@ -89,6 +123,24 @@
                                     $side_column = 'division_id';
                                     $area_type = 'App\Models\Division';
                                     $area_string = 'tarafa ya ';
+
+                                    $area =  App\Http\Controllers\Super\AreasController::search_for_area(
+                                        $relation_table,
+                                        $side_column,
+                                        $side_value,
+                                        $leader_id,
+                                        $area_type
+                                    );
+                                    $council = $area->council;
+                                    $district = $area->district;
+                                    $region = $area->region;
+                                    $area_stack = 'mkoa - '.$region->name.' | ';
+                                    $area_stack .= 'wilaya - '.$district->name.' | ';
+                                    $area_stack .= 'halmashauri - '.$council->name.' | ';
+                                    if($area){
+                                        echo "<p>".$area_stack."</p>";
+                                        echo $area_string.' <b>'.$area->name.'</b>';
+                                    }
                                 @endphp
                             @endif
 
@@ -98,6 +150,22 @@
                                     $side_column = 'council_id';
                                     $area_type = 'App\Models\Council';
                                     $area_string = 'halmashauri ya ';
+
+                                    $area =  App\Http\Controllers\Super\AreasController::search_for_area(
+                                        $relation_table,
+                                        $side_column,
+                                        $side_value,
+                                        $leader_id,
+                                        $area_type
+                                    );
+                                    $district = $area->district;
+                                    $region = $area->region;
+                                    $area_stack = 'mkoa - '.$region->name.' | ';
+                                    $area_stack .= 'wilaya - '.$district->name.' | ';
+                                    if($area){
+                                        echo "<p>".$area_stack."</p>";
+                                        echo $area_string.' <b>'.$area->name.'</b>';
+                                    }
                                 @endphp
                             @endif
 
@@ -107,6 +175,20 @@
                                     $side_column = 'district_id';
                                     $area_type = 'App\Models\District';
                                     $area_string = 'wilaya ya ';
+
+                                    $area =  App\Http\Controllers\Super\AreasController::search_for_area(
+                                        $relation_table,
+                                        $side_column,
+                                        $side_value,
+                                        $leader_id,
+                                        $area_type
+                                    );
+                                    $region = $area->region;
+                                    $area_stack = 'mkoa - '.$region->name.' | ';
+                                    if($area){
+                                        echo "<p>".$area_stack."</p>";
+                                        echo $area_string.' <b>'.$area->name.'</b>';
+                                    }
                                 @endphp
                             @endif
 
@@ -115,7 +197,7 @@
                                     $relation_table = 'leader_region';
                                     $side_column = 'region_id';
                                     $area_type = 'App\Models\Region';
-                                    $area_string = 'mkoa wa';
+                                    $area_string = 'mkoa wa simiyu';
                                 @endphp
                             @endif
 
